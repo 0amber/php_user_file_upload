67e6c16fefbfe9b10d9645a4cc6bb7a0b2b4d1d9
Kotaro Shimada <kotaroshimada38@gmail.com>
2014-02-12 17:43:22 +0900

    index.php ファイルアップロードまで変更 Signed-off-by: Kotaro Shimada <kotaroshimada38@gmail.com>

--- index.php
+++ index.php
@@ -9,23 +9,18 @@ require_once('./_libs/UIBase.class.php');
 *indexクラス
 *
 */
-
 $UIBaseObj = new UIBase();
 $AuthObj = new Auth();
 
-
 //セッションユーザ確認
-if (isset($_SESSION['name']) && isset($_SESSION['passwd'])) { 
-	if($AuthObj->user($_SESSION['name'], $_SESSION['passwd'])) {
-		if($AuthObj->admin($_SESSION['name'], $_SESSION['passwd']))
-
-
-		}
+if (!isset($_SESSION['name']) || !isset($_SESSION['passwd'])) header('Location: index.php');
+if($AuthObj->user($_SESSION['name'], $_SESSION['passwd'])) {
+		//管理者確認
+		if($AuthObj->admin($_SESSION['name'], $_SESSION['passwd'])) {
+			
 	}
 }
 
-
-$UIBaseObj = new UIBase();
 $UIBaseObj->display("index.tpl");
 
 
